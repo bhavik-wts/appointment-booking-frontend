8c93bab56dc5323456cfb9e4742eb72896287071
Bhavik <dalsaniyabhavik@gmail.com>
2025-02-23 12:14:48 +0530

    changes regarding .env

--- src/api/api.js
+++ src/api/api.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
-const LOGIN_URL = "http://localhost:5000/admin";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 export const bookAppointment = async (data) => {
-  return await axios.post(`${API_URL}/appointments`, data);
+  return await axios.post(`${API_BASE_URL}/api/appointments`, data);
 };
 
-export const adminLogin = (data) => axios.post(`${LOGIN_URL}/login`, data);
+export const adminLogin = (data) =>
+  axios.post(`${API_BASE_URL}/admin/login`, data);
 
 export const getAppointments = async (token) => {
-  return await axios.get(`${API_URL}/appointments`, {
+  return await axios.get(`${API_BASE_URL}/api/appointments`, {
     headers: { Authorization: token },
   });
 };
